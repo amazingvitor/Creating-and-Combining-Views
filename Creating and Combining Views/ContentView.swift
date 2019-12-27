691bdc99f7123e6f8f1b885c6f8de4c7ee335c9a
Vitor Hugo <vitor.h8@icloud.com>
2019-12-27 07:22:19 -0300

    Initial Commit

---
+++ Creating and Combining Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Creating and Combining Views
+//
+//  Created by Vitor Hugo on 27/12/19.
+//  Copyright © 2019 Vitor Hugo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
